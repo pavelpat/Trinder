7c1d42ea020aa894a0b2d964b0d4d7106bfe65b4
Павел Патрин <p.patrin@corp.mail.ru>
2016-03-29 10:53:47 +0300

    ~ Client service factory.

--- services/auth.js
+++ services/auth.js
@@ -1,7 +1,7 @@
-(function (chrome, $, App, Client) {
+(function (chrome, $, App) {
     'use strict';
 
-    App.service('Auth', function(){
+    App.service('Auth', function(Client){
         var authPromise = null;
 
         this.singleAuth = function () {
@@ -10,82 +10,82 @@
             }
             return authPromise;
         };
-    });
 
-    function cache() {
-        return new Promise(function (resolve, reject) {
-            chrome.storage.local.get('token', function (items) {
-                if (items.token && items.token.expiresAt > Date.now()) {
-                    resolve(items.token.token);
-                } else {
-                    chrome.storage.local.remove('token');
-                    reject();
-                }
-            });
-        });
-    }
-
-    function authorize() {
-        return new Promise(function (resolve, reject) {
-            // Send request to background script.
-            chrome.runtime.sendMessage({
-                type: 'request'
-            });
-
-            // Wait response from background script.
-            chrome.runtime.onMessage.addListener(function (message) {
-                if (message.type != 'response') {
-                    return;
-                }
-
-                // Cache token.
-                chrome.storage.local.set({
-                    token: {
-                        expiresAt: message.expires * 1000 + Date.now(),
-                        token: message.token
+        function cache() {
+            return new Promise(function (resolve, reject) {
+                chrome.storage.local.get('token', function (items) {
+                    if (items.token && items.token.expiresAt > Date.now()) {
+                        resolve(items.token.token);
+                    } else {
+                        chrome.storage.local.remove('token');
+                        reject();
                     }
                 });
-
-                resolve(message.token);
             });
-        });
-    }
+        }
 
-    function credentials(token, expires) {
-        return new Promise(function (resolve, reject) {
-            $.ajax('https://graph.facebook.com/v2.5/me', {
-                data: {
-                    'access_token': token
-                },
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept': 'application/json'
-                }
-            }).then(function (response) {
-                resolve({
-                    id: response.id,
-                    token: token
+        function authorize() {
+            return new Promise(function (resolve, reject) {
+                // Send request to background script.
+                chrome.runtime.sendMessage({
+                    type: 'request'
                 });
-            }, function () {
-                reject('Could not get profile info');
-            });
-        });
-    }
 
-    function connect(credentials) {
-        return new Promise(function (resolve, reject) {
-            var client = new Client(
-                credentials.id,
-                credentials.token
-            );
-            client.auth().then(function (person) {
-                resolve({
-                    client: client,
-                    person: person
+                // Wait response from background script.
+                chrome.runtime.onMessage.addListener(function (message) {
+                    if (message.type != 'response') {
+                        return;
+                    }
+
+                    // Cache token.
+                    chrome.storage.local.set({
+                        token: {
+                            expiresAt: message.expires * 1000 + Date.now(),
+                            token: message.token
+                        }
+                    });
+
+                    resolve(message.token);
                 });
-            }, function (e) {
-                reject('Could not authenticate: ' + e)
             });
-        });
-    }
-})(chrome, jQuery, App, Client);
+        }
+
+        function credentials(token, expires) {
+            return new Promise(function (resolve, reject) {
+                $.ajax('https://graph.facebook.com/v2.5/me', {
+                    data: {
+                        'access_token': token
+                    },
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Accept': 'application/json'
+                    }
+                }).then(function (response) {
+                    resolve({
+                        id: response.id,
+                        token: token
+                    });
+                }, function () {
+                    reject('Could not get profile info');
+                });
+            });
+        }
+
+        function connect(credentials) {
+            return new Promise(function (resolve, reject) {
+                var client = new Client(
+                    credentials.id,
+                    credentials.token
+                );
+                client.auth().then(function (person) {
+                    resolve({
+                        client: client,
+                        person: person
+                    });
+                }, function (e) {
+                    reject('Could not authenticate: ' + e)
+                });
+            });
+        }
+    });
+})(chrome, jQuery, App);
---
+++ services/client.js
@@ -0,0 +1,217 @@
+(function (chrome, $, App) {
+    'use strict';
+
+    App.factory('Client', function () {
+        var Client = function (fbId, fbToken) {
+            this.apiUrl = 'https://api.gotinder.com/';
+            this.authToken = null;
+            this.fbId = fbId;
+            this.fbToken = fbToken;
+        };
+
+        Client.prototype.auth = function () {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: self.apiUrl + 'auth',
+                    type: 'post',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    data: JSON.stringify({
+                        'facebook_id': self.fbId,
+                        'facebook_token': self.fbToken
+                    })
+                }).success(function (r) {
+                    self.authToken = r.token;
+                    resolve({
+                        name: r.user.full_name,
+                        bio: r.user.bio
+                    });
+                }).error(function (r) {
+                    self.authToken = null;
+                    reject('Bad response');
+                });
+            });
+        };
+
+        Client.prototype.meta = function () {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                reject('Not implemented');
+            });
+        };
+
+        Client.prototype.ping = function () {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                reject('Not implemented');
+            });
+        };
+
+        Client.prototype.pass = function (id) {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                if (!self.authToken) {
+                    reject('Not authenticated')
+                }
+                $.ajax({
+                    url: self.apiUrl + 'pass/' + id,
+                    type: 'get',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': self.authToken
+                    }
+                }).success(function (r) {
+                    resolve(true);
+                }).error(function (r) {
+                    reject('Bad response');
+                });
+            });
+        };
+
+        Client.prototype.like = function (id) {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                if (!self.authToken) {
+                    reject('Not authenticated')
+                }
+                $.ajax({
+                    url: self.apiUrl + 'like/' + id,
+                    type: 'get',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': self.authToken
+                    }
+                }).success(function (r) {
+                    resolve(r.match);
+                }).error(function (r) {
+                    reject('Bad response');
+                });
+            });
+        };
+
+        Client.prototype.recs = function () {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                if (!self.authToken) {
+                    reject('Not authenticated')
+                }
+                $.ajax({
+                    url: self.apiUrl + 'user/recs',
+                    type: 'get',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': self.authToken
+                    }
+                }).success(function (r) {
+                    resolve(r.results.map(function (r) {
+                        return {
+                            id: r._id,
+                            name: r.name,
+                            bio: r.bio,
+                            birthDate: r.birth_date,
+                            photos: r.photos.map(function (photo) {
+                                var photos = {
+                                    url: photo.url
+                                };
+                                photo.processedFiles.forEach(function (photo) {
+                                    photos[photo.width] = {
+                                        width: photo.width,
+                                        height: photo.height,
+                                        url: photo.url
+                                    }
+                                });
+
+                                return photos;
+                            })
+                        };
+                    }));
+                }).error(function (r) {
+                    reject('Bad response');
+                });
+            });
+        };
+
+        Client.prototype.send = function (id, message) {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: self.apiUrl + 'user/matches/' + id,
+                    type: 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': self.authToken
+                    },
+                    data: JSON.stringify({
+                        'message': message
+                    })
+                }).success(function (r) {
+                    resolve(true);
+                }).error(function (r) {
+                    reject('Bad response');
+                });
+            });
+
+        };
+
+        Client.prototype.updates = function (since) {
+            var self = this;
+            return new Promise(function (resolve, reject) {
+                $.ajax({
+                    url: self.apiUrl + 'updates',
+                    type: 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': self.authToken
+                    },
+                    data: JSON.stringify({
+                        'last_activity_date': since.toISOString()
+                    })
+                }).success(function (r) {
+                    resolve({
+                        lastActivity: r.last_activity_date,
+                        matches: r.matches.map(function (match) {
+                            return {
+                                id: match._id,
+                                person: match.person ? {
+                                    id: match.person._id,
+                                    name: match.person.name,
+                                    bio: match.person.bio,
+                                    birthDate: match.person.birth_date,
+                                    photos: match.person.photos.map(function (photo) {
+                                        var photos = {
+                                            url: photo.url
+                                        };
+                                        photo.processedFiles.forEach(function (photo) {
+                                            photos[photo.width] = {
+                                                width: photo.width,
+                                                height: photo.height,
+                                                url: photo.url
+                                            }
+                                        });
+
+                                        return photos;
+                                    })
+                                } : null,
+                                messages: match.messages.map(function (message) {
+                                    return {
+                                        to: message.to,
+                                        from: message.from,
+                                        message: message.message,
+                                        sent: message.sent_date
+                                    };
+                                }),
+                                lastActivity: match.last_activity_date
+                            };
+                        })
+                    });
+                }).error(function (r) {
+                    reject('Bad response');
+                });
+            });
+        };
+
+        return Client;
+    });
+})(chrome, jQuery, App);
