c0533887528d2916d559b1b73a58dad0b68b8d8b
Pavel Patrin <pavelpat@ya.ru>
2016-04-09 03:32:41 +0300

    Matches

--- controllers/matches.js
+++ controllers/matches.js
@@ -12,23 +12,21 @@
             this.cache = new Cache();
             this.client = null;
 
+            $scope.loading = true;
             ReadyClient.then((client, profile) => {
-                this.initClient(client, profile);
-                this.initGallery();
-                this.initDialog();
+                $scope.profile = profile;
+                $scope.loading = false;
+                $scope.$apply();
+
+                this.client = client;
+                // this.initGallery();
+                // this.initDialog();
+
+                this.client.updates(new Date(2012)).then((updates) => {
+                    $scope.matches = updates.matches;
+                    $scope.$apply();
+                });
             });
-        }
-
-        initClient(client, profile) {
-            let $s = this.scope;
-
-            this.cache.profile = profile;
-            this.scope.ready = client;
-            this.scope.person = profile;
-
-            // this.scope.refresh();
-
-            this.scope.$apply();
         }
 
         initDialog() {
@@ -99,75 +97,72 @@
             };
         }
 
-        update() {
-            Promise.all([
-                this.cache.activity,
-                this.cache.matches
-            ]).then((activity, matches) => {
-                return this.client.updates(activity).then((updates) => {
-                    this.cache.activity = updates.lastActivity;
-                    this.cache.matches = this.merge(matches, updates.matches);
-                });
-            });
-        }
-
-        merge(cached, fetched) {
-            // Take last activity date.
-            cached.lastActivity = fetched.activity;
-
-            // There is already ordered data in cache.
-            if (!fetched.matches.length) {
-                return cached;
-            }
-
-            // Merge matches items with order.
-            for (let match of fetched.matches) {
-                var lastActivity = new Date(match.activity);
-
-                // Sort messages by date.
-                match.messages.sort((a, b) => new Date(b.sent) - new Date(a.sent));
-
-                // Insert match to correct place.
-                if (cached.matches.length) {
-                    var replaced = false;
-
-                    // Replace existent match.
-                    for (var i = 0; i < cached.matches.length; i++) {
-                        if (match.id == cached.matches[i].id) {
-                            cached.matches[i] = match;
-                            replaced = true;
-                            break;
-                        }
-                    }
-
-                    // Insert new match.
-                    if (!replaced) for (var j = 0; j < cached.matches.length; j++) {
-                        var currCachedActivity = new Date(cached.matches[j].activity),
-                            prevCachedActivity = (j != 0) ? new Date(cached.matches[j - 1].activity) : null;
-
-                        if (
-                            // Merge first element.
-                        (lastActivity > currCachedActivity && prevCachedActivity === null) ||
-
-                        // Merge middle element.
-                        (lastActivity > currCachedActivity && lastActivity <= prevCachedActivity)
-                        ) {
-                            cached.matches.splice(j, 0, match);
-                            break;
-                        } else if (
-                            // Merge last element.
-                        lastActivity < currCachedActivity && j == cached.matches.length - 1
-                        ) {
-                            cached.matches.splice(j + 1, 0, match);
-                            break;
-                        }
-                    }
-                } else {
-                    cached.matches.push(match);
-                }
-            }
-
-            return cached;
-        }
+        // update() {
+        //     // Promise.all([
+        //     //     this.cache.activity,
+        //     //     this.cache.matches
+        //     // ]).then((activity, matches) => {
+        //     //
+        //     // });
+        // }
+        //
+        // merge(cached, fetched) {
+        //     // Take last activity date.
+        //     cached.lastActivity = fetched.activity;
+        //
+        //     // There is already ordered data in cache.
+        //     if (!fetched.matches.length) {
+        //         return cached;
+        //     }
+        //
+        //     // Merge matches items with order.
+        //     for (let match of fetched.matches) {
+        //         var lastActivity = new Date(match.activity);
+        //
+        //         // Sort messages by date.
+        //         match.messages.sort((a, b) => new Date(b.sent) - new Date(a.sent));
+        //
+        //         // Insert match to correct place.
+        //         if (cached.matches.length) {
+        //             var replaced = false;
+        //
+        //             // Replace existent match.
+        //             for (var i = 0; i < cached.matches.length; i++) {
+        //                 if (match.id == cached.matches[i].id) {
+        //                     cached.matches[i] = match;
+        //                     replaced = true;
+        //                     break;
+        //                 }
+        //             }
+        //
+        //             // Insert new match.
+        //             if (!replaced) for (var j = 0; j < cached.matches.length; j++) {
+        //                 var currCachedActivity = new Date(cached.matches[j].activity),
+        //                     prevCachedActivity = (j != 0) ? new Date(cached.matches[j - 1].activity) : null;
+        //
+        //                 if (
+        //                     // Merge first element.
+        //                 (lastActivity > currCachedActivity && prevCachedActivity === null) ||
+        //
+        //                 // Merge middle element.
+        //                 (lastActivity > currCachedActivity && lastActivity <= prevCachedActivity)
+        //                 ) {
+        //                     cached.matches.splice(j, 0, match);
+        //                     break;
+        //                 } else if (
+        //                     // Merge last element.
+        //                 lastActivity < currCachedActivity && j == cached.matches.length - 1
+        //                 ) {
+        //                     cached.matches.splice(j + 1, 0, match);
+        //                     break;
+        //                 }
+        //             }
+        //         } else {
+        //             cached.matches.push(match);
+        //         }
+        //     }
+        //
+        //     return cached;
+        // }
     });
 })(App);
