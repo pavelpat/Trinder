9888d9b29a55abe3942bb14fcc84a76a05f8a18f
Pavel Patrin <pavelpat@ya.ru>
2016-06-15 11:42:32 +0300

    Geolocation

--- controllers/locator.js
+++ controllers/locator.js
@@ -1,6 +1,6 @@
 ((ng, App) => {
     'use strict';
-    
+
     App.config(($stateProvider) => {
         $stateProvider.state('locator', {
             url: '/locator/:personId',
@@ -14,8 +14,10 @@
         /**
          * @param $scope
          * @param $stateParams
+         * @param SettingsStore
+         * @param Geo
          */
-        constructor($scope, $stateParams) {
+        constructor($scope, $stateParams, SettingsStore, Geo) {
             $scope.location = {
                 defaults: {
                     minZoom: 2,
@@ -36,7 +38,52 @@
             };
 
             $scope.detect = (id) => {
-                // debugger;
+                var client = $scope.client;
+
+                $scope.location.paths = {};
+
+                defaultPos().then((pos) => {
+                    // Jump to default point.
+                    return jump(pos[0], pos[1]).then(() => {
+                        return client.person(id).then((person) => {
+                            $scope.location.paths.r1 = {
+                                type: 'circle',
+                                radius: person.distance * 1000,
+                                latlngs: {lat: pos[0], lng: pos[1]}
+                            };
+                            $scope.$apply();
+                            return [pos[0], pos[1] + person.distance * 3 / 111.3];
+                        });
+                    });
+                }).then((pos) => {
+                    // Jump to second point.
+                    return jump(pos[0], pos[1]).then(() => {
+                        return client.person(id).then((person) => {
+                            $scope.location.paths.r2 = {
+                                type: 'circle',
+                                radius: person.distance * 1000,
+                                latlngs: {lat: pos[0], lng: pos[1]}
+                            };
+                            $scope.$apply();
+                            return [pos[0] + person.distance * 3 / 111, pos[1]];
+                        });
+                    });
+                }).then((pos) => {
+                    // Jump to third point.
+                    return jump(pos[0], pos[1]).then(() => {
+                        return client.person(id).then((person) => {
+                            $scope.location.paths.r3 = {
+                                type: 'circle',
+                                radius: person.distance * 1000,
+                                latlngs: {lat: pos[0], lng: pos[1]}
+                            };
+                            $scope.$apply();
+                        });
+                    });
+                }).then(() => {
+                    // Restore default point.
+                    defaultPos().then((pos) => jump(pos[0], pos[1]));
+                })
             };
 
             $scope.$watch('client', (value) => {
@@ -44,6 +91,29 @@
                     $scope.detect($stateParams.personId);
                 }
             });
+
+            function jump(lat, lon) {
+                var client = $scope.client;
+                // Change location sensitive (but not major).
+                return client.ping(lat + 0.000001, lon + 0.000001).then(() => () => {
+                    return client.ping(lat, lon);
+                });
+            }
+
+            function defaultPos() {
+                return SettingsStore.settings.then((settings) => {
+                    if (settings.geolocation) {
+                        // Browser selected location.
+                        return Geo.position();
+                    } else if (settings.geolat && settings.geolon) {
+                        // User selected location.
+                        return [settings.geolat, settings.geolon];
+                    } else {
+                        // Unknown location.
+                        return [0, 0];
+                    }
+                });
+            }
         }
     });
 })(angular, App);
